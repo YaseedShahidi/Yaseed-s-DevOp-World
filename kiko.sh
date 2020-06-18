b5123c3a183360bb9679de2f4324c537a713fdad
YaseedShahidi <yaseedshahidi@gmail.com>
2020-06-18 18:23:51 +0200

    My First Update

--- kiko.sh
+++ kiko.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-echo "Enter Your Name:"
-read name
-echo "Welcome $name To My World you are years old!"
+#!/bin/bash -x
+
+echo "Enter username"
+read username
+echo "Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "secret" ) ]]; then
+echo "valid user"
+else
+echo "invalid user"
+fi
