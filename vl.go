b113f99d4e35642a2a98de13a4753a6fcd6b3156
root <root@pop-os.localdomain>
2020-11-10 14:57:12 +0200

    GoLang Basics

---
+++ vl.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println("C.R.E.A.M, The Fresh Prince of Saint Martin!")
+
+    fmt.Println("Ashanti + Sammie =", 2*50)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    fmt.Println("Number One!", true || false)
+    fmt.Println("One of One!", true || false)
+    fmt.Println("The Only Fucking One!", true || false)
+}
